f21699b1b09cfb2736814a60949b90f7ddbbae04
paul <pavel.matyukov@gmail.com>
2015-11-04 16:44:21 +0300

    Inline style tag rule extractor

--- lib/phabeo.ex
+++ lib/phabeo.ex
@@ -1,42 +1,47 @@
 defmodule Phabeo do
-
-  defp list_it(list) do
-    list 
-    |> Tuple.to_list 
-    |> Enum.each(fn(item)-> 
-      IO.puts "element"
-      if is_list(item) or is_tuple(item) do
-        list_it item
-      else
-        item 
-      end
-    end)
+  
+  def render(template, rule_extractor \\ &rules_from_inner_style/1 ) do
+    parsed_template = Floki.parse(template)
+    rules = rule_extractor.(parsed_template)
+    complete_template = search_by_rules(parsed_template, rules)
+    Floki.raw_html complete_template
   end
 
-  def render(template) do
-    style = template
-    |> Floki.parse
-    |> Floki.find("style") 
-   
+  defp rules_from_inner_style(parsed_template) do
+    style = Floki.find(parsed_template, "style") 
+
     {_,_,value} = style |> List.first
-    
+
     clear_css_string = List.first(value)  
     |> String.replace(~r/\r?\n|\r/, "")
     |> String.replace(~r/\s+/, " ")
-    
-    rules = Regex.scan(~r/([^\{]*){([^\}]*)}/, clear_css_string, capture: :all_but_first)
-    
-    template_list = Floki.parse(template) |> list_it 
 
-    #IO.inspect template_list
-
-    Enum.each(rules, fn(rule) ->
-      [selector, value] = rule 
-      value = Floki.find(template, selector) 
-      #IO.inspect value
-    end)
+    Regex.scan(~r/([^\{]*){([^\}]*)}/, clear_css_string, capture: :all_but_first)
 
   end
-  
+
+  defp update_attr({el, attr, body}, new_attr) do
+    {el, attr ++ [new_attr], body }
+  end
+
+  defp list_it(list, old, style_value) do
+    case list do
+      {el, attr, [head | tail]} -> 
+        if [head] == old do 
+          head = update_attr(head, {"style", style_value})
+        end 
+        [{el, attr, [head | list_it(tail, old, style_value)]}]
+      [el] -> list_it(el, old, style_value) 
+      [] -> [] 
+    end
+  end
+   
+  defp search_by_rules(template, rules) do
+    Enum.reduce rules, template, fn(rule,acc)->
+      [selector, value] = rule
+      should_inlined = Floki.find(template, selector)
+      list_it(acc, should_inlined, value)
+    end
+  end
 
 end
